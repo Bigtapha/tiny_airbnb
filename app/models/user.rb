c208162c6e1c549afd5eb0fa398e1ed7bff12e46
Juliette Guerbette <turtleju@gmail.com>
2019-11-11 18:02:04 +0100

    model Tiny House and DB Tiny House, User modified for foreign_key owner

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :rents
+  has_many :tiny_houses, through: :rents # locataire
+  has_many :own_tiny_houses, class_name: 'TinyHouse', foreign_key: :owner_id
 end
+
+# User.last.tiny_houses     => tiny_houses locataire
+# User.last.own_tiny_houses => tiny_houses proprio
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_144755) do
+ActiveRecord::Schema.define(version: 2019_11_11_161044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tiny_houses", force: :cascade do |t|
+    t.bigint "owner_id"
+    t.string "title"
+    t.string "location"
+    t.integer "travellers_number"
+    t.integer "price"
+    t.string "description"
+    t.integer "bed_number"
+    t.integer "bedroom_number"
+    t.string "wifi"
+    t.string "kitchen"
+    t.string "sauna"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_tiny_houses_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +44,5 @@ ActiveRecord::Schema.define(version: 2019_11_11_144755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tiny_houses", "users", column: "owner_id"
 end
