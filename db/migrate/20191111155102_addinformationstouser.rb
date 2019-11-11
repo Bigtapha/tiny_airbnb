c6e278b80373ebb8be84cc0f27e67d932ea07991
tieuf <ctapha13@gmail.com>
2019-11-11 17:00:50 +0100

    add informations to users

---
+++ db/migrate/20191111155102_addinformationstouser.rb
@@ -0,0 +1,7 @@
+class Addinformationstouser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string 
+    add_column :users, :telephon, :integer
+    add_column :users, :description, :string
+  end
+end
